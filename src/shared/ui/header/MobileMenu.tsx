108927d40cdc361024f1dff218ea7ab71f8fecf5
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-12-05 18:34:38 +0900

    Add MobileMenu component for responsive navigation

---
+++ src/shared/ui/header/MobileMenu.tsx
@@ -0,0 +1,103 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { ReactNode } from "react";
+
+export interface MobileMenuItem {
+  label: string;
+  icon: ReactNode;
+  onClick?: () => void;
+  divider?: boolean;
+  styles?: Record<string, unknown>;
+}
+
+export interface MobileMenuProps {
+  menuItems: MobileMenuItem[];
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  hide?: boolean;
+  iconColor?: string;
+}
+
+const MenuList = ({
+  menuItems,
+  onClose,
+}: {
+  menuItems: MobileMenuItem[];
+  onClose: () => void;
+}) => (
+  <Box
+    sx={{ width: 250 }}
+    role="presentation"
+    onClick={onClose}
+    onKeyDown={onClose}
+  >
+    <List>
+      {menuItems.map((item, index) => (
+        <div key={`${item.label}-${index}`}>
+          <ListItem disablePadding>
+            <ListItemButton
+              sx={{
+                ...(item.styles || {}),
+                "&:hover, &:focus": {
+                  backgroundColor: "transparent",
+                },
+              }}
+              onClick={item.onClick || (() => {})}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.label} />
+            </ListItemButton>
+          </ListItem>
+          {item.divider && <Divider />}
+        </div>
+      ))}
+    </List>
+    <Divider />
+  </Box>
+);
+
+const MobileMenu = ({
+  menuItems,
+  isOpen,
+  onOpen,
+  onClose,
+  hide = false,
+  iconColor = "white",
+}: MobileMenuProps) => {
+  if (hide) {
+    return null;
+  }
+
+  return (
+    <Box
+      sx={{
+        textAlign: "right",
+        display: { xs: "block", md: "none" },
+      }}
+    >
+      <IconButton onClick={onOpen}>
+        <MenuIcon
+          sx={{
+            color: iconColor,
+          }}
+        />
+      </IconButton>
+      <Drawer anchor="right" open={isOpen} onClose={onClose}>
+        <MenuList menuItems={menuItems} onClose={onClose} />
+      </Drawer>
+    </Box>
+  );
+};
+
+export default MobileMenu;
