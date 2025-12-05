1401c3c0c174ce78d6dab0b5f18bb5c1db95fcd9
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-12-05 17:44:15 +0900

    Improve import statement for useState in DownloadForm component

--- src/components/download_form/DownloadForm.tsx
+++ src/components/download_form/DownloadForm.tsx
@@ -2,7 +2,7 @@ import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOu
 import { Box, Chip, CircularProgress, Stack } from "@mui/material";
 import { DesktopDatePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
-import React, { useState } from "react";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
