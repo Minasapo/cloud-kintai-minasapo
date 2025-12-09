b4f842718ef19f401fe901bee2b53f7b253233f9
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-12-09 15:25:51 +0900

    Update import path for updateStaff to use shared API structure

--- src/hooks/useStaffs/updateStaffData.ts
+++ src/hooks/useStaffs/updateStaffData.ts
@@ -1,8 +1,9 @@
 import { GraphQLResult } from "@aws-amplify/api";
 import { API } from "aws-amplify";
 
+import { updateStaff } from "@shared/api/graphql/documents/mutations";
+
 import { Staff, UpdateStaffInput, UpdateStaffMutation } from "../../API";
-import { updateStaff } from "../../graphql/mutations";
 
 export default async function updateStaffData(input: UpdateStaffInput) {
   const response = (await API.graphql({
