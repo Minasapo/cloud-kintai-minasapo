0f47aaceb993c644ad1a71442c60c8caf5604d44
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-12-09 14:29:06 +0900

    Update import statements for attendance and calendar APIs to use absolute paths for better organization

--- src/components/AttendanceDailyList/AttendanceDailyList.tsx
+++ src/components/AttendanceDailyList/AttendanceDailyList.tsx
@@ -1,5 +1,10 @@
 import "./styles.scss";
 
+import { useLazyListRecentAttendancesQuery } from "@entities/attendance/api/attendanceApi";
+import {
+  useGetCompanyHolidayCalendarsQuery,
+  useGetHolidayCalendarsQuery,
+} from "@entities/calendar/api/calendarApi";
 import {
   Alert,
   AlertTitle,
@@ -25,11 +30,6 @@ import { Attendance } from "@/API";
 import { useAppDispatchV2 } from "@/app/hooks";
 import MoveDateItem from "@/components/AttendanceDailyList/MoveDateItem";
 import { AppConfigContext } from "@/context/AppConfigContext";
-import { useLazyListRecentAttendancesQuery } from "@/entities/attendance/api/attendanceApi";
-import {
-  useGetCompanyHolidayCalendarsQuery,
-  useGetHolidayCalendarsQuery,
-} from "@/entities/calendar/api/calendarApi";
 import * as MESSAGE_CODE from "@/errors";
 import { AttendanceDate } from "@/lib/AttendanceDate";
 import { setSnackbarError } from "@/lib/reducers/snackbarReducer";
