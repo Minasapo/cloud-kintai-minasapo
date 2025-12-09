47f9e3afddbd201804b620e92f4125660fbe6ab7
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-12-09 15:28:04 +0900

    Update import path for getWorkflow to use shared API structure

--- src/pages/Workflow/WorkflowDetail.tsx
+++ src/pages/Workflow/WorkflowDetail.tsx
@@ -10,6 +10,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import { getWorkflow } from "@shared/api/graphql/documents/queries";
 import StatusChip from "@shared/ui/chips/StatusChip";
 import Page from "@shared/ui/page/Page";
 import { API } from "aws-amplify";
@@ -28,7 +29,6 @@ import {
 } from "@/API";
 import { useAppDispatchV2 } from "@/app/hooks";
 import { AuthContext } from "@/context/AuthContext";
-import { getWorkflow } from "@/graphql/queries";
 import useStaffs from "@/hooks/useStaffs/useStaffs";
 import useWorkflows from "@/hooks/useWorkflows/useWorkflows";
 import { formatDateSlash, isoDateFromTimestamp } from "@/lib/date";
