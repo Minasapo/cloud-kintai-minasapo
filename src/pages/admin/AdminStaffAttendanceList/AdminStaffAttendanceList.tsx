54038b1d4770001a8d6e9dbb3190de7c1f141d8d
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-12-09 13:07:12 +0900

    Update import paths for attendanceApi to enhance module organization

--- src/pages/admin/AdminStaffAttendanceList/AdminStaffAttendanceList.tsx
+++ src/pages/admin/AdminStaffAttendanceList/AdminStaffAttendanceList.tsx
@@ -34,13 +34,13 @@ import {
 } from "@/API";
 import handleApproveChangeRequest from "@/components/attendance_editor/ChangeRequestDialog/handleApproveChangeRequest";
 import { AttendanceStatusTooltip } from "@/components/AttendanceList/AttendanceStatusTooltip";
-import createOperationLogData from "@/hooks/useOperationLog/createOperationLogData";
-import fetchStaff from "@/hooks/useStaff/fetchStaff";
-import { mappingStaffRole, StaffType } from "@/hooks/useStaffs/useStaffs";
 import {
   useListRecentAttendancesQuery,
   useUpdateAttendanceMutation,
-} from "@/lib/api/attendanceApi";
+} from "@/entities/attendance/api/attendanceApi";
+import createOperationLogData from "@/hooks/useOperationLog/createOperationLogData";
+import fetchStaff from "@/hooks/useStaff/fetchStaff";
+import { mappingStaffRole, StaffType } from "@/hooks/useStaffs/useStaffs";
 import {
   useGetCompanyHolidayCalendarsQuery,
   useGetHolidayCalendarsQuery,
