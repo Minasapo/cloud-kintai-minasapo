1a6724d75dfb4967779c7876ee01e441ffaedc4b
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2026-01-27 15:26:47 +0900

    Organize exports in AdminShiftSettings for improved clarity and structure

--- src/pages/admin/AdminShiftSettings/index.ts
+++ src/pages/admin/AdminShiftSettings/index.ts
@@ -1,1 +1,12 @@
+export { createShiftGroup, createShiftGroupId } from "./shiftGroupFactory";
+export { default as ShiftGroupRow } from "./ShiftGroupRow";
 export { SHIFT_GROUP_TEXTS } from "./shiftGroupTexts";
+export {
+  getGroupValidation,
+  getHelperTexts,
+  type GroupHelperTexts,
+  type GroupValidationResult,
+  parseOptionalInteger,
+  type ShiftGroupFormValue,
+} from "./shiftGroupValidation";
+export { default as useShiftGroupValidation } from "./useShiftGroupValidation";
